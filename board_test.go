c801050d2befa0fd6b4747cd56cb3f1b097c55c5
Nat Welch <nat@natwelch.com>
2018-05-02 03:43:55 +0000

    Add multiple games

--- board_test.go
+++ board_test.go
@@ -1,18 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 	"testing"
 )
 
 func TestParse(t *testing.T) {
-	file, err := ioutil.ReadFile("test.ptn")
+	files, err := ioutil.ReadDir("./test_games")
 	if err != nil {
 		t.Errorf("%+v", err)
 	}
 
-	_, err = ParsePTN(file)
-	if err != nil {
-		t.Errorf("%+v", err)
+	for _, fi := range files {
+		file, err := ioutil.ReadFile(fmt.Sprintf("test_games/%s", fi.Name()))
+		if err != nil {
+			t.Errorf("%+v", err)
+		}
+
+		_, err = ParsePTN(file)
+		if err != nil {
+			t.Errorf("%+v", err)
+		}
 	}
 }
