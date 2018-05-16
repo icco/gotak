914228ebe41eea9a5f28eacb5e3b816f6ca0e506
Nat Welch <nat@natwelch.com>
2018-05-16 02:21:38 +0000

    progress

--- scanner.go
+++ scanner.go
@@ -4,6 +4,50 @@ import (
 	"io"
 )
 
+// Represents a lexical token
+type Token int
+
+// Represent the kinds of tokens we can have
+const (
+	ILLEGAL Token = iota
+	EOF
+	WS
+	NEWLINE
+
+	SPACE
+	COUNT
+	DIRECTION
+	STONE
+
+	INFORMATION
+	COMMENT_START
+	TEXT
+	COMMENT_END
+)
+
+// Represents a token type and the literal characters it was parsed from.
+type TokenInstance struct {
+	Type    Token
+	Literal string
+}
+
+// Returns true if ch is a space or a tab.
+func isWhitespace(ch rune) bool {
+	return ch == ' ' || ch == '\t'
+}
+
+// Returns true if ch is a upper or lowercase letter.
+func isLetter(ch rune) bool {
+	return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')
+}
+
+// Returns true if ch is a numeric digit
+func isDigit(ch rune) bool {
+	return (ch >= '0' && ch <= '9')
+}
+
+var eof = rune(0)
+
 // Scanner represents a lexical scanner.
 type Scanner struct {
 	r *bufio.Reader
