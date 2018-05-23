e1c8e55e6c8f5592bc18c4516e8764dbf63be8dd
Nat Welch <nat@natwelch.com>
2018-05-23 03:14:55 +0000

    Cap stones

--- move_test.go
+++ move_test.go
@@ -13,6 +13,7 @@ func TestParseMove(t *testing.T) {
 		"2a2+2",
 		"3a3+3",
 		"4a4>121",
+		"1a4+1C",
 	}
 
 	for _, mv := range tests {
@@ -25,6 +26,44 @@ func TestParseMove(t *testing.T) {
 
 			if m.Stone == "" {
 				t.Errorf("stone is empty")
+			}
+
+			if m.Square == "" {
+				t.Errorf("square is empty")
+			}
+
+			if m.MoveCount == 0 {
+				t.Errorf("move count is zero")
+			}
+
+			if m.MoveDirection == "" {
+				t.Errorf("direction is empty")
+			}
+
+			if len(m.MoveDropCounts) < 1 {
+				t.Errorf("drop counts is empty")
+			}
+		})
+	}
+}
+
+func TestParseMoveCapStones(t *testing.T) {
+	tests := []string{
+		"1e6+1C",
+		"1e6+C",
+		"4e6+1111C",
+	}
+
+	for _, mv := range tests {
+		t.Run(mv, func(t *testing.T) {
+
+			m, err := NewMove(mv)
+			if err != nil {
+				t.Errorf("error creating move: %+v", err)
+			}
+
+			if m.Stone != StoneCap {
+				t.Errorf("stone is wrong")
 			}
 
 			if m.Square == "" {
