466d86206b49e5fb90b3fbdfeb08f72d17cd5610
Nat Welch <nat@natwelch.com>
2018-05-23 03:17:21 +0000

    more tests

--- move_test.go
+++ move_test.go
@@ -84,3 +84,53 @@ func TestParseMoveCapStones(t *testing.T) {
 		})
 	}
 }
+
+func TestParseDropCapStones(t *testing.T) {
+	tests := []string{
+		"Ce6",
+	}
+
+	for _, mv := range tests {
+		t.Run(mv, func(t *testing.T) {
+
+			m, err := NewMove(mv)
+			if err != nil {
+				t.Errorf("error creating move: %+v", err)
+			}
+
+			if m.Stone != StoneCap {
+				t.Errorf("stone is wrong")
+			}
+
+			if m.Square == "" {
+				t.Errorf("square is empty")
+			}
+		})
+	}
+}
+
+func TestParseDropStones(t *testing.T) {
+	tests := []string{
+		"e6",
+		"Fe6",
+		"b10",
+	}
+
+	for _, mv := range tests {
+		t.Run(mv, func(t *testing.T) {
+
+			m, err := NewMove(mv)
+			if err != nil {
+				t.Errorf("error creating move: %+v", err)
+			}
+
+			if m.Stone != StoneFlat {
+				t.Errorf("stone is wrong")
+			}
+
+			if m.Square == "" {
+				t.Errorf("square is empty")
+			}
+		})
+	}
+}
