11ee08e3bbd30aa42edd6a5e645182d6562f0b9d
Nat Welch <nat@natwelch.com>
2018-05-25 02:29:01 +0000

    Basic

--- web/main.go
+++ web/main.go
@@ -11,5 +11,11 @@ func main() {
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("welcome"))
 	})
+
+	r.Get("/game/{id}/?", func(w http.ResponseWriter, r *http.Request) { w.Write([]byte("welcome")) })
+	r.Get("/game/{id}/{move}/?", func(w http.ResponseWriter, r *http.Request) { w.Write([]byte("welcome")) })
+	r.Post("/game/{id}/move/?", func(w http.ResponseWriter, r *http.Request) { w.Write([]byte("welcome")) })
+	r.Post("/game/new", func(w http.ResponseWriter, r *http.Request) { w.Write([]byte("welcome")) })
+
 	http.ListenAndServe(":3000", r)
 }
