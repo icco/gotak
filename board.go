339df901fac5844cd51b37f8e6a17f9b7e2f32a4
Nat Welch <nat@natwelch.com>
2018-06-14 04:37:32 +0000

    spacing

--- board.go
+++ board.go
@@ -98,25 +98,24 @@ func (b *Board) IsEdge(l string) bool {
 // FindRoad starts at square l and uses a flood fill algorithm to find a road.
 //
 // The flood fill algorithm we use is based on the following:
-// ```
-// Flood-fill (node, target-color, replacement-color):
-//   1. If target-color is equal to replacement-color, return.
-//   2. If color of node is not equal to target-color, return.
-//   3. Set Q to the empty queue.
-//   4. Add node to Q.
-//   5. For each element N of Q:
-//   6.     Set w and e equal to N.
-//   7.     Move w to the west until the color of the node to the west of w no
-//            longer matches target-color.
-//   8.     Move e to the east until the color of the node to the east of e no
-//            longer matches target-color.
-//   9.     For each node n between w and e:
-//  10.         Set the color of n to replacement-color.
-//  11.         If the color of the node to the north of n is target-color, add that node to Q.
-//  12.         If the color of the node to the south of n is target-color, add that node to Q.
-//  13. Continue looping until Q is exhausted.
-//  14. Return.
-// ```
+//
+//  Flood-fill (node, target-color, replacement-color):
+//    1. If target-color is equal to replacement-color, return.
+//    2. If color of node is not equal to target-color, return.
+//    3. Set Q to the empty queue.
+//    4. Add node to Q.
+//    5. For each element N of Q:
+//    6.     Set w and e equal to N.
+//    7.     Move w to the west until the color of the node to the west of w no
+//             longer matches target-color.
+//    8.     Move e to the east until the color of the node to the east of e no
+//             longer matches target-color.
+//    9.     For each node n between w and e:
+//   10.         Set the color of n to replacement-color.
+//   11.         If the color of the node to the north of n is target-color, add that node to Q.
+//   12.         If the color of the node to the south of n is target-color, add that node to Q.
+//   13. Continue looping until Q is exhausted.
+//   14. Return.
 func (b *Board) FindRoad(l string) bool {
 	queue := []string{}
 	queue = append(queue, l)
