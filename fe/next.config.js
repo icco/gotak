e9a4832a5bb2b0845d1c5495ebe168e02f6b68db
Nat Welch <nat@natwelch.com>
2019-04-17 23:13:37 +0000

    lint

--- fe/next.config.js
+++ fe/next.config.js
@@ -1,3 +1,3 @@
 const withCSS = require("@zeit/next-css");
-const withSass = require('@zeit/next-sass')
-module.exports = withSass()
+const withSass = require("@zeit/next-sass");
+module.exports = withSass();
