920c3e96a9ead5def8249cb1c8872dc4e1978c3f
Nat Welch <nat@natwelch.com>
2019-04-17 23:22:03 +0000

    sure

--- fe/pages/board.js
+++ fe/pages/board.js
@@ -4,8 +4,246 @@ export default () => (
   <div id="viewer">
     <div className="view-wrapper">
       <div />
-      <div className="table-wrapper" />
-      <div />
+      <div class="table-wrapper">
+        <div class="table size-5">
+          <div class="middle">
+            <div class="center">
+              <div
+                class="board"
+                style={{ width: "551.852px", height: "551.852px" }}
+              >
+                <div class="squares">
+                  <div class="square c0 r0 light valid">
+                    <div class="road">
+                      <div class="up" />
+                      <div class="down" />
+                      <div class="left" />
+                      <div class="right" />
+                    </div>
+                    <div class="option-number" />
+                  </div>
+                  <div class="square c1 r0 dark valid">
+                    <div class="road">
+                      <div class="up" />
+                      <div class="down" />
+                      <div class="left" />
+                      <div class="right" />
+                    </div>
+                    <div class="option-number" />
+                  </div>
+                  <div class="square c2 r0 light valid">
+                    <div class="road">
+                      <div class="up" />
+                      <div class="down" />
+                      <div class="left" />
+                      <div class="right" />
+                    </div>
+                    <div class="option-number" />
+                  </div>
+                  <div class="square c3 r0 dark valid">
+                    <div class="road">
+                      <div class="up" />
+                      <div class="down" />
+                      <div class="left" />
+                      <div class="right" />
+                    </div>
+                    <div class="option-number" />
+                  </div>
+                  <div class="square c4 r0 light valid">
+                    <div class="road">
+                      <div class="up" />
+                      <div class="down" />
+                      <div class="left" />
+                      <div class="right" />
+                    </div>
+                    <div class="option-number" />
+                  </div>
+                  <div class="square c0 r1 dark valid">
+                    <div class="road">
+                      <div class="up" />
+                      <div class="down" />
+                      <div class="left" />
+                      <div class="right" />
+                    </div>
+                    <div class="option-number" />
+                  </div>
+                  <div class="square c1 r1 light valid">
+                    <div class="road">
+                      <div class="up" />
+                      <div class="down" />
+                      <div class="left" />
+                      <div class="right" />
+                    </div>
+                    <div class="option-number" />
+                  </div>
+                  <div class="square c2 r1 dark valid">
+                    <div class="road">
+                      <div class="up" />
+                      <div class="down" />
+                      <div class="left" />
+                      <div class="right" />
+                    </div>
+                    <div class="option-number" />
+                  </div>
+                  <div class="square c3 r1 light valid">
+                    <div class="road">
+                      <div class="up" />
+                      <div class="down" />
+                      <div class="left" />
+                      <div class="right" />
+                    </div>
+                    <div class="option-number" />
+                  </div>
+                  <div class="square c4 r1 dark valid">
+                    <div class="road">
+                      <div class="up" />
+                      <div class="down" />
+                      <div class="left" />
+                      <div class="right" />
+                    </div>
+                    <div class="option-number" />
+                  </div>
+                  <div class="square c0 r2 light valid">
+                    <div class="road">
+                      <div class="up" />
+                      <div class="down" />
+                      <div class="left" />
+                      <div class="right" />
+                    </div>
+                    <div class="option-number" />
+                  </div>
+                  <div class="square c1 r2 dark valid">
+                    <div class="road">
+                      <div class="up" />
+                      <div class="down" />
+                      <div class="left" />
+                      <div class="right" />
+                    </div>
+                    <div class="option-number" />
+                  </div>
+                  <div class="square c2 r2 light valid">
+                    <div class="road">
+                      <div class="up" />
+                      <div class="down" />
+                      <div class="left" />
+                      <div class="right" />
+                    </div>
+                    <div class="option-number" />
+                  </div>
+                  <div class="square c3 r2 dark valid">
+                    <div class="road">
+                      <div class="up" />
+                      <div class="down" />
+                      <div class="left" />
+                      <div class="right" />
+                    </div>
+                    <div class="option-number" />
+                  </div>
+                  <div class="square c4 r2 light valid">
+                    <div class="road">
+                      <div class="up" />
+                      <div class="down" />
+                      <div class="left" />
+                      <div class="right" />
+                    </div>
+                    <div class="option-number" />
+                  </div>
+                  <div class="square c0 r3 dark valid">
+                    <div class="road">
+                      <div class="up" />
+                      <div class="down" />
+                      <div class="left" />
+                      <div class="right" />
+                    </div>
+                    <div class="option-number" />
+                  </div>
+                  <div class="square c1 r3 light valid">
+                    <div class="road">
+                      <div class="up" />
+                      <div class="down" />
+                      <div class="left" />
+                      <div class="right" />
+                    </div>
+                    <div class="option-number" />
+                  </div>
+                  <div class="square c2 r3 dark active p2 valid placed">
+                    <div class="road">
+                      <div class="up" />
+                      <div class="down" />
+                      <div class="left" />
+                      <div class="right" />
+                    </div>
+                    <div class="option-number" />
+                  </div>
+                  <div class="square c3 r3 light valid">
+                    <div class="road">
+                      <div class="up" />
+                      <div class="down" />
+                      <div class="left" />
+                      <div class="right" />
+                    </div>
+                    <div class="option-number" />
+                  </div>
+                  <div class="square c4 r3 dark valid">
+                    <div class="road">
+                      <div class="up" />
+                      <div class="down" />
+                      <div class="left" />
+                      <div class="right" />
+                    </div>
+                    <div class="option-number" />
+                  </div>
+                  <div class="square c0 r4 light valid">
+                    <div class="road">
+                      <div class="up" />
+                      <div class="down" />
+                      <div class="left" />
+                      <div class="right" />
+                    </div>
+                    <div class="option-number" />
+                  </div>
+                  <div class="square c1 r4 dark valid">
+                    <div class="road">
+                      <div class="up" />
+                      <div class="down" />
+                      <div class="left" />
+                      <div class="right" />
+                    </div>
+                    <div class="option-number" />
+                  </div>
+                  <div class="square c2 r4 light valid">
+                    <div class="road">
+                      <div class="up" />
+                      <div class="down" />
+                      <div class="left" />
+                      <div class="right" />
+                    </div>
+                    <div class="option-number" />
+                  </div>
+                  <div class="square c3 r4 dark valid">
+                    <div class="road">
+                      <div class="up" />
+                      <div class="down" />
+                      <div class="left" />
+                      <div class="right" />
+                    </div>
+                    <div class="option-number" />
+                  </div>
+                  <div class="square c4 r4 light valid">
+                    <div class="road">
+                      <div class="up" />
+                      <div class="down" />
+                      <div class="left" />
+                      <div class="right" />
+                    </div>
+                    <div class="option-number" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 );
