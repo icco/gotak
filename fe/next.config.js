46b3e8102288b580644d5d3805998a5fa54b59ec
Nat Welch <nat@natwelch.com>
2019-04-17 23:12:46 +0000

    playing with ptn.ninja css

--- fe/next.config.js
+++ fe/next.config.js
@@ -1,2 +1,3 @@
 const withCSS = require("@zeit/next-css");
-module.exports = withCSS();
+const withSass = require('@zeit/next-sass')
+module.exports = withSass()
---
+++ fe/pages/board.js
@@ -0,0 +1,11 @@
+import "../css/board.scss";
+
+export default () => (
+          <div id="viewer">
+          <div className="view-wrapper">
+            <div></div>
+            <div className="table-wrapper"></div>
+            <div></div>
+          </div>
+          </div>
+);
